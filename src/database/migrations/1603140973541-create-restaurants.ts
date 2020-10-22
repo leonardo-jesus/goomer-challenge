e75a6db943238af1e1621fd1e77592d4781ea2e2
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-22 16:30:52 -0300

    Add Create Restaurant migration

---
+++ src/database/migrations/1603140973541-create-restaurants.ts
@@ -0,0 +1,49 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class createRestaurants1603140973541 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(new Table({
+        name: 'restaurants',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            unsigned: true,
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'latitude',
+            type: 'decimal',
+            scale: 10,
+            precision: 2,
+          },
+          {
+            name: 'longitude',
+            type: 'decimal',
+            scale: 10,
+            precision: 2,
+          },
+          {
+            name: 'about',
+            type: 'text',
+          },
+          {
+            name: 'opening_hours',
+            type: 'varchar',
+          }
+        ],
+      }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable('restaurants');
+    }
+
+}
