ae5a6f092e7cb84907ee7d42843d520a467d9ac9
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-22 16:26:58 -0300

    Add Product Model

---
+++ src/models/Product.ts
@@ -0,0 +1,42 @@
+import { Entity, Column, PrimaryGeneratedColumn, JoinColumn, ManyToOne, OneToMany } from 'typeorm';
+import Restaurant from './Restaurant';
+import ImageProduct from './ImageProduct';
+
+@Entity('products')
+export default class Product {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  price: number;
+
+  @Column()
+  category: string;
+
+  @Column()
+  offer: boolean;
+
+  @Column()
+  offer_description: string;
+
+  @Column()
+  offer_price: number;
+
+  @Column()
+  offer_length: string;
+
+  @ManyToOne(() => Restaurant, restaurant => restaurant.products, {
+    cascade: ['insert', 'update']
+  })
+  @JoinColumn({ name: 'product_id'})
+  restaurant: Restaurant[];
+
+  @OneToMany(() => ImageProduct, image => image.product, {
+    cascade: ['insert', 'update']
+  })
+  @JoinColumn({ name: 'product_id'})
+  images: ImageProduct[];
+}
