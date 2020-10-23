8fc888097dc32318516d7b2733e624c70bda0e6d
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-23 19:28:25 -0300

    Add Create Product function

--- src/controllers/RestaurantsController.ts
+++ src/controllers/RestaurantsController.ts
@@ -5,6 +5,7 @@ const isEmpty = require('lodash.isempty');
 import * as Yup from 'yup';
 
 import Restaurant from '../models/Restaurant';
+import Product from '../models/Product';
 import ImagesRestaurant from '../models/ImagesRestaurant';
 
 export default {
@@ -12,7 +13,7 @@ export default {
     const restaurantsRepository = getRepository(Restaurant);
 
     const restaurants = await restaurantsRepository.find({
-      relations: ['imagesRestaurant']
+      relations: ['imagesRestaurant', 'products']
     });
 
     return res.json(restaurants);
@@ -24,7 +25,7 @@ export default {
     const restaurantsRepository = getRepository(Restaurant);
 
     const restaurant = await restaurantsRepository.findOneOrFail(id, {
-      relations: ['imagesRestaurant']
+      relations: ['imagesRestaurant', 'products']
     });
 
     return res.json(restaurant);
@@ -150,5 +151,61 @@ export default {
     return res.status(200).json(restaurant);
   },
 
+  async createProduct(req: Request, res: Response) {
+    // const { id } = req.params;
 
+    const productsRepository = getRepository(Product);
+    // const restaurant = await restaurantsRepository.findOne(id);
+
+    const {
+      name,
+      price,
+      category,
+      offer,
+      offer_description,
+      offer_price,
+      offer_length
+    } = req.body;
+
+    const reqImages = req.files as Express.Multer.File[];
+    const imagesProduct = reqImages.map(image => {
+      return { path: image.filename }
+    });
+
+    const data = {
+      name,
+      price,
+      category,
+      offer,
+      offer_description,
+      offer_price,
+      offer_length,
+      imagesProduct
+    };
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      price: Yup.number().required(),
+      category: Yup.string().required(),
+      offer: Yup.boolean().required(),
+      offer_description: Yup.string().max(300),
+      offer_price: Yup.number(),
+      offer_length: Yup.string(),
+      imagesProduct: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required()
+        })
+      )
+    });
+
+    await schema.validate(data, {
+      abortEarly: false,
+    });
+
+    const product = productsRepository.create(data);
+
+    await productsRepository.save(product);
+
+    return res.status(201).json(product);
+  }
 };
