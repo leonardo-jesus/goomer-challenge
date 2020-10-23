f3477fa83dcf27f4a3d4547f725ca17ee60b2db1
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-23 19:27:46 -0300

    Add Create Product route

--- src/routes.ts
+++ src/routes.ts
@@ -10,7 +10,7 @@ routes.get('/restaurants', RestaurantsController.indexRestaurant);
 routes.get('/restaurants/:id', RestaurantsController.showRestaurant);
 
 routes.post('/restaurants', upload.array('imagesRestaurant'), RestaurantsController.createRestaurant);
-// routes.post('/restaurants/:id/products', upload.array('imagesProduct'), RestaurantsController.createProduct)
+routes.post('/restaurants/:id/products', upload.array('imagesProduct'), RestaurantsController.createProduct)
 
 routes.patch('/restaurants/:id', upload.array('imagesRestaurant'), RestaurantsController.updateRestaurant);
 
